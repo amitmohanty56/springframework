6498671e205556ec3b4300c0f38e3a9f13314d94
Amit Mohanty <amitmohanty56@gmail.com>
2021-01-02 11:30:54 +0800

    non-select operation

--- springDaoBasics/src/TestClient.java
+++ springDaoBasics/src/TestClient.java
@@ -2,7 +2,7 @@ import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.springjdbc.dao.EmployeeDao;
+import com.springjdbc.dao.EmployeeDao_NonSelectOperation;
 
 public class TestClient {
 
@@ -11,11 +11,19 @@ public class TestClient {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("dispatcher-servlet.xml");
 		
-		EmployeeDao dao = context.getBean("employeeDao",EmployeeDao.class);
-		dao.updateEmployee();
-		System.out.println(" Employee Details Updated ");
-		//dao.indertEmployee();
+		EmployeeDao_NonSelectOperation dao = context.getBean("employeeDao",EmployeeDao_NonSelectOperation.class);
+		//dao.updateEmployee();
+		//System.out.println(" Employee Details Updated ");
+		//dao.insertEmployee();
 		//System.out.println(" Employee Details Inserted ");
+		//dao.insertEmployeeDynamicQuery("arpita", "arpita2020");
+		//System.out.println(" Employee Details Inserted dynamically");
+		//dao.insertEmployeeDynamicQueryWithVarArgObj("jacob", "jacob2021");
+		//System.out.println(" Employee Details Inserted dynamically using vararg argument");
+		//dao.insertEmployeeDynamicQueryWithVarArgObjAndParameterType("stephan", "stephan123");
+		//System.out.println(" Employee Details Inserted dynamically using input argument tyep");
+		dao.updateEmployeeUsingPreparedStatementSetter("arpita", "arpita2021");
+		System.out.println(" Employee Details updated dynamically using PreparedStatementSetter ");
 
 	}
 
